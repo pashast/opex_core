8062d1a1c7953475f31adcefff11619a8391dc3f
pashast <pasha.strus@gmail.com>
2023-02-27 23:57:30 +0200

    add uk-ua lang

---
+++ opex.ocmod/admin/language/uk-ua/other/opexcore.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title'] = 'Opencart.express (основні файли)';
+
+// Text
+$_['text_extension'] = 'Розширення';
+$_['text_success']   = 'Налаштування успішно змінено!';
+$_['text_edit']      = 'Налаштування модуля';
+
+// Entry
+$_['entry_status']        = 'Статус';
+$_['entry_ac_limit']      = 'Ліміт автодоповнення';
+$_['entry_ac_full_limit'] = 'Ліміт автодоповнення у спливаючому вікні';
+
+// Error
+$_['error_permission'] = 'У Вас немає прав для керування цим модулем!';
